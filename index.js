c5c7ec040ecc925bbedbf0f82b47137c6c7754da
Paulina Salem <paulinasalem@yahoo.com>
2018-03-27 02:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ function logWhisper (string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma (string) {
-case string.toLowerCase:
+if string.toLowerCase():
 return "I can't hear you!"
-break;
-case string.toUpperCase:
+else if string.toUpperCase():
 return "YES INDEED!"
-break;
-case "I love you, Grandma.":
+else "I love you, Grandma.":
 return: "I love you, too."
 }
